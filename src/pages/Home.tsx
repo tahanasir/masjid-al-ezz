96d0e6360701885090b982a1053f6439cd84e80f
tahanasir <6380634+tahanasir@users.noreply.github.com>
2025-09-21 21:56:18 -0400

    hide donation section on mobile doesnt work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -147,7 +147,7 @@ export default function Home() {
           </div>
         </section>
         
-        <div className="relative z-10 w-full bg-primary min-h-[calc(100vh-80px)] py-12 md:py-16"
+        <div className="hidden md:block relative z-10 w-full bg-primary min-h-[calc(100vh-80px)] py-12 md:py-16"
           style={{
             backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.05'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
           }}
